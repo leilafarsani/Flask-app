2326311b0aa3ff829d734abd1dbf11edd1dce913
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-17 01:00:29 +0000

    form submission

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
 
-def hello_world():
-    return 'Hello, World!'
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
