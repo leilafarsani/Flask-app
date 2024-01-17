284bd92dc73cdffcc2232aea1cfab8e96485add2
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-17 01:21:42 +0000

    organizing

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form['name']
-    return f'Hello {name}!'
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
